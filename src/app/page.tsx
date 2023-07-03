7dc19d905e0e7f629ce8dc0965ca88c689526979
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-03 14:03:24 +0530

    add: configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import MainImage from "./components/MainImage"
+import MainImage from '../components/MainImage';
 
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center">
       <MainImage />
     </main>
-  )
+  );
 }
