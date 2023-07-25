a5b68493a23ef030273ae91c234883bc33b060b2
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-25 17:58:23 +0530

    add new blogs flow

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,9 +1,24 @@
+import { Button } from 'antd';
 import Layout from 'components/common/Layout';
+import routes from 'config/routes';
+import { useNavigate } from 'react-router-dom';
 
 function Blogs() {
+  const navigate = useNavigate();
+
   return (
     <Layout page="Blogs">
-      <div className="flex justify-center items-center h-full">Blogs</div>
+      <div className="flex h-full">
+        {/* create account and other actions */}
+        <div className="flex justify-end w-full p-4">
+          <Button
+            className="cpbutton"
+            onClick={() => navigate(routes.NEW_BLOG)}
+          >
+            Create Blog
+          </Button>
+        </div>
+      </div>
     </Layout>
   );
 }
